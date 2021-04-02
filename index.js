c66cb9c65f2d54b60aea0af7aacca89666112417
Gopal Paul <gopal.kumar.paul1@gmail.com>
2021-04-02 21:22:11 +0600

    Books Library Server

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const port = 5045;
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.5mhw5.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 
 
-app.listen(port)
+app.listen(process.env.PORT || port)
 
 
 
